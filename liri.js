021920353319ac9e843e3d1b9e79a1f0fdb9aabb
Todd Germaine <trgermaine@gmail.com>
2016-07-10 10:41:47 -0400

    have twitter and spotify functionality working

--- liri.js
+++ liri.js
@@ -2,10 +2,10 @@
 var keys = require('./keys.js');
 var Twitter = require('twitter');
 var fs = require('fs');
+var spotify = require('spotify');
 
 //Assigning Values to arguments in terminal
 var userInput1 = process.argv[2];
-var userInput2 = process.argv[3];
 
 //Assigned a new object using the exported keys object
 var client = new Twitter ({
@@ -17,17 +17,36 @@ var client = new Twitter ({
 
 //If else statement to determine what to print if typed in terminal by the user
 if (userInput1 === 'my-tweets') {
-    var params = { screen_name: 'KingJames' };
-    client.get('statuses/user_timeline', params, function(error, tweets, response){
-    if (!error) {
+    var userID = process.argv[3] || 'KingJames'
+    var params = { screen_name: userID };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
         for (var i = 0; i < 20; i++) {
-                console.log(tweets[i].text);
+                  console.log(tweets[i].text);
+        }
+      }
+    });
+
+} if (userInput1 === 'spotify-this-song') {
+    var userInput2 = process.argv[3] || 'what\'s my age again by Blink 182'
+    spotify.search({ type: 'track', query: userInput2 }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
     }
-  }
+
+      for (var i = 0; i < data.tracks.items.length; i++) {
+          console.log('song name: ' + data.tracks.items[i].name);
+          console.log('preview url:'+ data.tracks.items[i].preview_url);
+          console.log('album name: ' + data.tracks.items[i].album.name);
+
+        for( var k = 0; k < data.tracks.items[k].artists.length; k++) {
+             console.log('artist name: ' + data.tracks.items[i].artists[k].name);
+        }
+      }
+ 
+    // Do something with 'data' 
 });
-  console.log('i work');
-  // Do Twitter Function
-}if (userInput1 === 'spotify-this-song') {
 
   // Do Spotify Function
 } if (userInput1 === 'movie-this') {
@@ -37,4 +56,4 @@ if (userInput1 === 'my-tweets') {
 
   // Do Random Function
 }
- 
+
