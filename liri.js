42395420f2fa4f48e74e8920979232e5a757602f
Todd Germaine <trgermaine@gmail.com>
2016-07-10 13:01:41 -0400

    completed spotify and imdb functionality

--- liri.js
+++ liri.js
@@ -3,9 +3,11 @@ var keys = require('./keys.js');
 var Twitter = require('twitter');
 var fs = require('fs');
 var spotify = require('spotify');
+var request = require('request');
 
 //Assigning Values to arguments in terminal
 var userInput1 = process.argv[2];
+var userInput2 = process.argv[3];
 
 //Assigned a new object using the exported keys object
 var client = new Twitter ({
@@ -28,8 +30,9 @@ if (userInput1 === 'my-tweets') {
     });
 
 } if (userInput1 === 'spotify-this-song') {
-    var userInput2 = process.argv[3] || 'What\'s My Age Again by Blink 182'
-    spotify.search({ type: 'track', query: userInput2 }, function(err, data) {
+    var songInput = process.argv[3] || 'What\'s My Age Again by Blink 182'
+    spotify.search({ type: 'track', query: songInput }, function(err, data) {
+    
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
@@ -37,22 +40,50 @@ if (userInput1 === 'my-tweets') {
       for (var i = 0; i < 5; i++) {
             console.log('---------------------------------------');
             console.log('Song Name: ' + data.tracks.items[i].name);
-            console.log('Preview Link: ' + data.tracks.items[i].preview_url);
             console.log('Album Name: ' + data.tracks.items[i].album.name);
+            console.log('Preview Link: ' + data.tracks.items[i].preview_url);
 
         for( var k = 0; k < data.tracks.items[k].artists.length; k++) {
             console.log('Artist Name: ' + data.tracks.items[i].artists[k].name);
             console.log('---------------------------------------');
         }
       }
-}); 
+    }); 
 
-  // Do Spotify Function
 } if (userInput1 === 'movie-this') {
+    var movieInput = process.argv[3] || 'Mr. Nobody'
+    console.log(movieInput);
+    var queryURL = "http://www.omdbapi.com/?t=" + movieInput + "&y=&plot=small&r=json";
 
-  // Do Movie Function
+    request(queryURL, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+
+      // Variable to describe shorten the JSON Parse for the body
+      var format = JSON.parse(body);
+      console.log("Title: " + format.Title + 
+                  "\nYear: " + format.Year +
+                  "\nIMDB Rating:" + format.imdbRating +
+                  "\nCountry " + format.Country +
+                  "\nLaunguage: " + format.Launguage +
+                  "\nPlot: " + format.Plot +
+                  "\nActors: " + format.Actors); 
+      }
+    });
+
 } if (userInput1 === 'do-what-it-says') {
 
-  // Do Random Function
-}
+    fs.readFile('random.txt', 'utf8', function(err, data) {
+        if (err) {
 
+          console.log('Error Occurred' + err);
+          return;
+        } else 
+          
+          data = data.split(',');
+            userInput1 = data[0];
+            userInput2 = data[1];
+
+            //Finish creating Spotify Function to push the text to use the spotify npm package
+            // spotifyFunction();
+    });
+}
